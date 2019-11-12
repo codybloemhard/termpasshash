6eafd7016ae9c66d1bd84baf05de7bd279c909bf
Cody Bloemhard <cblmhrd@live.nl>
2019-11-12 21:52:14 +0100

    clean up and truncate

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,34 @@ use sha3::{Sha3_512, Digest};
 use term_basics_linux as tbl;
 
 fn main() {
-    //tbl::println_cols_style("TermPassHash", tbl::UserColour::Cyan, tbl::UserColour::Std, tbl::TextStyle::Bold);
     tbl::set_style(tbl::TextStyle::Bold);
     tbl::println_col("TermPassHash", tbl::UserColour::Magenta);
-    tbl::use_colour(tbl::UserColour::Cyan, tbl::FGBG::FG);
+    tbl::set_colour(tbl::UserColour::Cyan, tbl::FGBG::FG);
     let password = prompt_secure("Password: ");
     let salt = prompt_secure("Salt: ");
-    let rounds;
+    let rounds = prompt_until_correct("Rounds: ");
+    let mlen: usize = prompt_until_correct("Max chars: ");
+    let mut res = secure_hash(password, salt, rounds);
+    res.truncate(mlen);
+    print_masked(res, tbl::UserColour::Magenta);
+}
+
+fn print_masked<T: std::fmt::Display>(msg: T, col: tbl::UserColour){
+    tbl::use_style(tbl::TextStyle::Std);
+    tbl::println_cols(msg, col.clone(), col);
+    tbl::restore_style();
+}
+
+fn prompt_until_correct<T: std::str::FromStr>(msg: &str) -> T{
     loop{
-        let str_rounds = prompt_secure("Rounds: ");
-        let x = tbl::string_to_value(&str_rounds);
+        let string = prompt_secure(msg);
+        let x: Option<T> = tbl::string_to_value(&string);
         if let Some(xv) = x {
-            rounds = xv;
-            break;
+            return xv;
+        }else{
+            tbl::println_col(" > could not parse", tbl::UserColour::Red);
         }
     }
-    let res = secure_hash(password, salt, rounds);
-    tbl::println_cols(res, tbl::UserColour::Magenta, tbl::UserColour::Magenta);
 }
 
 fn prompt_secure(msg: &str) -> String{
