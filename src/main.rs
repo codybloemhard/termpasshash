b8988f7cf9f5615ae190b879d81359b7c4603665
Cody Bloemhard <cblmhrd@live.nl>
2019-11-12 23:22:48 +0100

    command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,53 @@
 use sha3::{Sha3_512, Digest};
 use term_basics_linux as tbl;
+use lapp;
+use std::cmp::{max};
 
 fn main() {
+    let args = lapp::parse_args("
+        A program to hash your passwords.
+        You can use the hash it self as a password for websites, etc.
+        -r,--rounds (default 0) //How many rounds of hashing we will do. 0 means you will be prompted for it.
+        -l,--length (default 0) //How long the final output is, maximum. 0 means you will be prompted for it.
+        -u,--unmask //Show the resulting hash in readable colours, instead of the masked version.
+        -p,--password (default '') //The password to be hashed. If '' you will be prompted for it. Be carefull using this flag: never show the password as plaintext on screen.
+        -s,--salt (default '') //The salt to be used. If '' you will be promted for it. Be carefull using this flag if you want your salt to be secret.
+    ");
+    let arg_rounds = args.get_integer("rounds");
+    let arg_length = args.get_integer("length");
+    let arg_unmask = args.get_bool("unmask");
+    let arg_password = args.get_string("password");
+    let arg_salt = args.get_string("salt");
     tbl::set_style(tbl::TextStyle::Bold);
     tbl::println_col("TermPassHash", tbl::UserColour::Magenta);
     tbl::set_colour(tbl::UserColour::Cyan, tbl::FGBG::FG);
-    let password = prompt_secure("Password: ");
-    let salt = prompt_secure("Salt: ");
-    let rounds = prompt_until_correct("Rounds: ");
-    let mlen: usize = prompt_until_correct("Max chars: ");
+    let password = if arg_password == ""{
+        prompt_secure("Password: ")
+    }else{
+        arg_password
+    };
+    let salt = if arg_salt == ""{
+        prompt_secure("Salt: ")
+    }else{
+        arg_salt
+    };
+    let rounds: usize = if arg_rounds == 0{
+        prompt_until_correct("Rounds: ")
+    }else{
+        max(1, arg_rounds as usize)
+    };
+    let mlen: usize = if arg_length == 0{
+        prompt_until_correct("Max chars: ")
+    }else{
+        arg_length as usize
+    };
     let mut res = secure_hash(password, salt, rounds);
     res.truncate(mlen);
-    print_masked(res, tbl::UserColour::Magenta);
+    if arg_unmask{
+        tbl::println(res);
+    }else{
+        print_masked(res, tbl::UserColour::Magenta);
+    }
 }
 
 fn print_masked<T: std::fmt::Display>(msg: T, col: tbl::UserColour){
